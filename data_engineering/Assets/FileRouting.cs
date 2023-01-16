38c2e54bf9ba35eca17b603ad854cd3188d5ec8b
Michał <michal.skrzypiec@capgemini.com>
2023-01-16 21:15:12 +0100

    added simple parkingzone with reprocessing

--- data_engineering/Assets/FileRouting.cs
+++ data_engineering/Assets/FileRouting.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class FileRouting : MonoBehaviour
 {
     public GameObject fileRouting;
+    public GameObject parkingzone;
     public float movementSpeed;
     private Dictionary<int, Vector3> checkpointsPositions;
     private int checkpointsPassed;
+    private bool inParkingzone;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        inParkingzone = false;
+
         checkpointsPositions = new Dictionary<int, Vector3>();
         int cnt = 1;
         foreach (Transform childTransform in fileRouting.transform)
@@ -31,19 +35,41 @@ public class FileRouting : MonoBehaviour
     {
         if (checkpointsPositions.Count > checkpointsPassed)
         {
-            Vector3 startPoint = checkpointsPositions[checkpointsPassed];
-            Vector3 endPoint = checkpointsPositions[checkpointsPassed + 1];
-            Vector3 direction = (endPoint - startPoint).normalized;
-
-            transform.position += (direction * movementSpeed) * Time.deltaTime;
-
-            float pathToPass = (endPoint - startPoint).magnitude;
-            float pathPassed = (transform.position - startPoint).magnitude;
-
-            if (pathPassed >= pathToPass)
+            if (inParkingzone)
             {
-                transform.position = endPoint;
-                checkpointsPassed += 1;
+                if (Input.GetKeyDown(KeyCode.R))
+                {
+                    checkpointsPassed = 1;
+                    transform.position = checkpointsPositions[checkpointsPassed];
+                    inParkingzone = false;
+                    Debug.Log("reprocessed");
+                }
+            }
+            else
+            {
+                if (Input.GetKeyDown(KeyCode.F))
+                {
+                    transform.position = parkingzone.transform.position;
+                    inParkingzone = true;
+                    Debug.Log("moved to parkingzone");
+                }
+                else
+                {
+                    Vector3 startPoint = checkpointsPositions[checkpointsPassed];
+                    Vector3 endPoint = checkpointsPositions[checkpointsPassed + 1];
+                    Vector3 direction = (endPoint - startPoint).normalized;
+
+                    transform.position += (direction * movementSpeed) * Time.deltaTime;
+
+                    float pathToPass = (endPoint - startPoint).magnitude;
+                    float pathPassed = (transform.position - startPoint).magnitude;
+
+                    if (pathPassed >= pathToPass)
+                    {
+                        transform.position = endPoint;
+                        checkpointsPassed += 1;
+                    }
+                }
             }
         }
     }
