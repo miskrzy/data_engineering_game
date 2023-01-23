235a4eaa038b767c558c83aa9d1739a7d597fa36
Michał <michal.skrzypiec@capgemini.com>
2023-01-23 21:51:16 +0100

    added empty conveyor object and class

---
+++ data_engineering/Assets/ConveyorProcessor.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorProcessor : Processor
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override bool canConsume()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override bool canProvide()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void consumeElement(GameObject element)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override GameObject provideElement()
+    {
+        throw new System.NotImplementedException();
+    }
+}
